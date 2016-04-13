b2730476aa1b9fdaf9d2372e6dce34553316992f
Sasha Kulek <kulek_alexsandr@mail.ru>
2016-04-13 08:19:43 +0300

    rewrite first letter of class n -> N, add constructors

--- src/main/java/com/goit/gojavaonline/tictactoe/negativeIntegerInputException.java
+++ src/main/java/com/goit/gojavaonline/tictactoe/negativeIntegerInputException.java
@@ -4,5 +4,11 @@ package com.goit.gojavaonline.tictactoe;
  * Created by User on 13.04.2016.
  */
 public class NegativeIntegerInputException extends Exception {
+    public NegativeIntegerInputException() {
+        super();
+    }
 
+    public NegativeIntegerInputException(String message) {
+        super(message);
+    }
 }
