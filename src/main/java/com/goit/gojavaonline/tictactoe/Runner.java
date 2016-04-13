9abb123e92dd06a5ac76c371694743a0c8dab6e0
Sasha Kulek <kulek_alexsandr@mail.ru>
2016-04-13 22:32:29 +0300

    remove 'sout' from main

--- src/main/java/com/goit/gojavaonline/tictactoe/Runner.java
+++ src/main/java/com/goit/gojavaonline/tictactoe/Runner.java
@@ -9,6 +9,5 @@ public class Runner {
         Board b = new Board();
         Game game = new Game(b);
         game.startGame();
-        System.out.println(game.getGameResult()); // TODO: 13/04/2016 move this line to cass Game
     }
 }
