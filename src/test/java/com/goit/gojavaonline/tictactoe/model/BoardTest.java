28faa63f16d797ff78a441f9c71136ba4869bfbd
Sasha Kulek <kulek_alexsandr@mail.ru>
2016-04-18 23:24:14 +0300

    add test for method getEmptyCells

--- src/test/java/com/goit/gojavaonline/tictactoe/model/BoardTest.java
+++ src/test/java/com/goit/gojavaonline/tictactoe/model/BoardTest.java
@@ -1,59 +1,59 @@
 package com.goit.gojavaonline.tictactoe.model;
 
-import com.goit.gojavaonline.tictactoe.model.Board;
-import com.goit.gojavaonline.tictactoe.model.CellContent;
 import com.goit.gojavaonline.tictactoe.view.ConsoleUserInteraction;
 import com.goit.gojavaonline.tictactoe.view.UserInteraction;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.util.List;
+
 
 /**
  * Created by tamila on 4/11/16.
  */
 public class BoardTest {
-
     private static Board board;
     private static UserInteraction userInteraction;
 
     @BeforeClass
-    public static void setUpClass(){
+    public static void setUpClass() {
         board = new Board();
         userInteraction = new ConsoleUserInteraction();
     }
 
-    private void setHorizontalLine(int rowNumber, CellContent cellContent){
+    private void setHorizontalLine(int rowNumber, CellContent cellContent) {
         board.clearBoard();
-        for (int i = 0; i < board.getCells()[rowNumber].length; i++){
+        for (int i = 0; i < board.getCells()[rowNumber].length; i++) {
             board.getCells()[rowNumber][i].setContent(cellContent);
         }
     }
-    private void setVerticalLine(int columnNumber, CellContent cellContent){
+
+    private void setVerticalLine(int columnNumber, CellContent cellContent) {
         board.clearBoard();
-        for(int i = 0; i < board.getCells().length; i++){
+        for (int i = 0; i < board.getCells().length; i++) {
             board.getCells()[i][columnNumber].setContent(cellContent);
         }
     }
 
-    private void setDiagonalLine1(CellContent cellContent){
+    private void setDiagonalLine1(CellContent cellContent) {
         board.clearBoard();
-        for(int i = 0; i < board.getCells().length; i++){
+        for (int i = 0; i < board.getCells().length; i++) {
             board.getCells()[i][i].setContent(cellContent);
         }
     }
 
-    private void setDiagonalLine2(CellContent cellContent){
+    private void setDiagonalLine2(CellContent cellContent) {
         board.clearBoard();
         int rowIndex = 0;
-        for(int i = board.getCells().length - 1; i >= 0; i--){
+        for (int i = board.getCells().length - 1; i >= 0; i--) {
             board.getCells()[rowIndex++][i].setContent(cellContent);
         }
     }
 
     @Test
     public void testHorizontalLinesIsWin() throws Exception {
-        for(int i = 0; i < board.getCells().length; i++){
+        for (int i = 0; i < board.getCells().length; i++) {
             setHorizontalLine(i, CellContent.CROSS);
             userInteraction.drawBoard(board);
 
@@ -62,10 +62,9 @@ public class BoardTest {
         }
     }
 
-
     @Test
     public void testVerticalLinesIsWin() throws Exception {
-        for(int i = 0; i < board.getCells()[0].length; i++){
+        for (int i = 0; i < board.getCells()[0].length; i++) {
             setVerticalLine(i, CellContent.CROSS);
             userInteraction.drawBoard(board);
 
@@ -88,22 +87,33 @@ public class BoardTest {
     }
 
     @Test
-    public void clearBoard() throws Exception {
+    public void clearBoard() {
         for (int i = 0; i < board.DIMENSION; i++) {
-            board.setUpCellContent(i,i,CellContent.CROSS);
+            board.setUpCellContent(i, i, CellContent.CROSS);
         }
 
         board.clearBoard();
 
         for (int i = 0; i < board.DIMENSION; i++) {
             for (int j = 0; j < board.DIMENSION; j++) {
-                Assert.assertEquals(CellContent.EMPTY,board.getCells()[i][j].getContent());
+                Assert.assertEquals(CellContent.EMPTY, board.getCells()[i][j].getContent());
             }
         }
 
 
     }
 
+    @Test
+    public void getEmptyCells() throws Exception {
+        board.setUpCellContent(0, 0, CellContent.CROSS);
+        board.setUpCellContent(1, 1, CellContent.ZERO);
+        board.setUpCellContent(2, 2, CellContent.EMPTY);
+
+        List<Cell> emptyCells = board.getEmptyCells();
+
+        Assert.assertNotNull(emptyCells);
+        Assert.assertEquals("size of list is wrong",(board.DIMENSION * board.DIMENSION) - 2, emptyCells.size());
+    }
 
 }
 
